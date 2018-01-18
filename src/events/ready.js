b5519d83de7f0a3abf69528dc1c9ad3ab4f79ab8
Will <31250645+LumiteDubbz@users.noreply.github.com>
2018-01-18 07:20:35 +0000

    setGame deprecated, change to set activity.

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,5 +3,5 @@ const client = require('../structures/Client.js');
 client.on('ready', () => {
   console.log('I have connected: Ready to serve ' + client.guilds.size + ' servers with ' + client.users.size + ' members!');
 
-  client.user.setGame('on ' + client.guilds.size + ' servers', 'https://twitch.tv/lumitedubbz');
+  client.user.setActivity('on ' + client.guilds.size + ' servers', {url: 'https://twitch.tv/lumitedubbz', type: 'STREAMING'});
 });

6d21ec5f0ced591aa3b02eb23db63bd86fcd4024
Will <31250645+LumiteDubbz@users.noreply.github.com>
2018-01-18 07:16:33 +0000

    setGame deprecated, change to set activity.

--- src/commands/botowner/Game.js
+++ src/commands/botowner/Game.js
@@ -24,7 +24,7 @@ class Game extends Command {
   async run(msg, args, text) {
     await msg.client.user.setActivity(args.game, {url: 'https://twitch.tv/lumitedubbz', type: 'STREAMING'});
 
-    return text.reply('successfully set my game to ' + args.game + '! To change it again, type ' + msg.client.config.prefix + 'game command.');
+    return text.reply('successfully set my game to ' + args.game + '! To change it again, type the `' + msg.client.config.prefix + 'game` command.');
   }
 }
 

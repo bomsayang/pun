42d7405934bab8bf5e0111bd0be35ba10e7b3c5e
Will <lumitedubbz@gmail.com>
2018-03-01 18:15:29 +0000

    Warn command. Pixel is officially finished until I can learn how to make the automod and fighting system which won't be for a while.

---
+++ src/commands/moderator/Warn.js
@@ -0,0 +1,38 @@
+const { Command, Argument } = require('patron.js');
+const utility = require('../../utility');
+const ModerationService = require('../../services/ModerationService')
+
+class Warn extends Command {
+  constructor() {
+    super({
+      names: ['warn'],
+      groupName: 'moderator',
+      description: 'Warn any member!',
+      botPermissions: ['MANAGE_ROLES'],
+      args: [
+        new Argument({
+          name: 'member',
+          key: 'member',
+          type: 'member',
+          preconditions: ['nomoderator'],
+          example: '"Jack Kannoff#0711"'
+        }),
+        new Argument({
+          name: 'warning',
+          key: 'warning',
+          type: 'string',
+          example: 'having a small willy',
+          remainder: true
+        })
+      ]
+    });
+  }
+
+  async run(msg, args, text) {
+    await text.reply('You have successfully warned ' + args.member.user.tag + ' for `' + args.warning + '`.');
+    await ModerationService.tryInformUser(msg.guild, msg.author, 'warned', args.member.user, args.warning);
+    await ModerationService.tryModLog(msg.dbGuild, msg.guild, 'Warn', utility.Constants.embedColors.mute, args.warning, msg.author, args.member.user);
+  }
+}
+
+module.exports = new Warn();

d8b391c6d0825b76fdee504240ca09762182e6be
Dafydd <25325299+DafsCode@users.noreply.github.com>
2018-03-07 12:37:57 +0000

    Added fancy text.

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,5 +4,12 @@ const logger = require('cus-log');
 
 client.on('ready', () => {
   client.user.setActivity(`on ${client.guilds.size} guilds | ${constants.defaultPrefix}help`, {url: 'https://twitch.tv/lumitedubbz', type: 'STREAMING'});
-  logger.log(1, 'Bot ready')
+  logger.log(1, '__________.__              .__   ')
+  logger.log(1, '\______   \__|__  ___ ____ |  |  ')
+  logger.log(1, ' |     ___/  \  \/  // __ \|  |  ')
+  logger.log(1, ' |    |   |  |>    <\  ___/|  |__')
+  logger.log(1, ' |____|   |__/__/\_ \\___  >____/')
+  logger.log(1, '                   \/    \/      ')
+  logger.log(1, '\n')
+  logger.log(1, 'Bot is ready to be used in ${client.guilds.size}!
 });
